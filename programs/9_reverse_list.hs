d8f994cf493c5a5ab8def49687341789c30454d8
Manav Patnaik <dev.manavpatnaik@gmail.com>
2022-01-19 15:36:35 +0530

    Some recursive programs

--- programs/9_reverse_list.hs
+++ programs/9_reverse_list.hs
@@ -2,5 +2,14 @@ reverseList :: [Int] -> [Int]
 reverseList [] = []
 reverseList (x:xs) = (reverseList xs) ++ [x]
 
+appendElement :: Int -> [Int] -> [Int]
+appendElement x [] = [x]
+appendElement x (list_head:list_tail) = list_head : appendElement x list_tail
+
+reverseListA :: [Int] -> [Int]
+reverseListA [] = []
+reverseListA (x:xs) = appendElement x (reverseListA xs)
+
 main = do
     print(reverseList [1,2,3,4,5])
+    print(reverseListA [1,2,3,4,5])
