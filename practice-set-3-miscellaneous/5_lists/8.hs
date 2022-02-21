bb4d09c75a2894469d870408be5b0051a225366c
Manav Patnaik <dev.manavpatnaik@gmail.com>
2022-02-21 19:56:25 +0530

    Probably the final commit

--- practice-set-3-miscellaneous/5_lists/8.hs
+++ practice-set-3-miscellaneous/5_lists/8.hs
@@ -1,6 +1,9 @@
-import Data.ByteString
-sumOddIndices :: [Int] -> Int
-sumOddIndices [] = 0
-sumOddIndices (x : xs)
-  | odd (findIndex x xs) = x + sumOddIndices xs
-  | otherwise = sumOddIndices xs
+sumOddIndices [] _ = 0
+sumOddIndices lst (-1) = 0
+sumOddIndices lst index
+    | odd index = (lst !! index) + sumOddIndices lst (index-1)
+    | otherwise = sumOddIndices lst (index-1)
+
+main = do
+    let lst = [1,2,3,4,5,6]
+    print(sumOddIndices lst (length lst - 1))
